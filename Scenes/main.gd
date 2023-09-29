e7ac9a2b7df1014bf1dd2001235f55e0ab90e1d8
Yurii Antonchyk <xsat.php@gmail.com>
2023-09-30 00:16:12 +0300

    Reformated mark scene

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -2,17 +2,19 @@ extends Node
 
 class_name Main
 
-@onready var local_player_x = $LocalPlayer_X as LocalPlayer
-@onready var local_player_o = $LocalPlayer_O as LocalPlayer
-@onready var mark_1_1 = $Grid/Mark_1_1 as Mark
-@onready var mark_1_2 = $Grid/Mark_1_2 as Mark
-@onready var mark_1_3 = $Grid/Mark_1_3 as Mark
-@onready var mark_2_1 = $Grid/Mark_2_1 as Mark
-@onready var mark_2_2 = $Grid/Mark_2_2 as Mark
-@onready var mark_2_3 = $Grid/Mark_2_3 as Mark
-@onready var mark_3_1 = $Grid/Mark_3_1 as Mark
-@onready var mark_3_2 = $Grid/Mark_3_2 as Mark
-@onready var mark_3_3 = $Grid/Mark_3_3 as Mark
+@onready var local_player_x: LocalPlayer = $LocalPlayers/LocalPlayer_X as LocalPlayer
+@onready var local_player_o: LocalPlayer = $LocalPlayers/LocalPlayer_O as LocalPlayer
+
+@onready var mark_0_0: Mark = $Grid/Mark_0_0 as Mark
+@onready var mark_0_1: Mark = $Grid/Mark_0_1 as Mark
+@onready var mark_0_2: Mark = $Grid/Mark_0_2 as Mark
+@onready var mark_1_0: Mark = $Grid/Mark_1_0 as Mark
+@onready var mark_1_1: Mark = $Grid/Mark_1_1 as Mark
+@onready var mark_1_2: Mark = $Grid/Mark_1_2 as Mark
+@onready var mark_2_0: Mark = $Grid/Mark_2_0 as Mark
+@onready var mark_2_1: Mark = $Grid/Mark_2_1 as Mark
+@onready var mark_2_2: Mark = $Grid/Mark_2_2 as Mark
+
 
 var player_value: String
 
@@ -21,15 +23,17 @@ func _ready() -> void:
 	local_player_o.active()
 	player_value = local_player_o.text
 	
+	mark_0_0.mark_pressed.connect(_on_mark_pressed)
+	mark_0_1.mark_pressed.connect(_on_mark_pressed)
+	mark_0_2.mark_pressed.connect(_on_mark_pressed)
+	
+	mark_1_0.mark_pressed.connect(_on_mark_pressed)
 	mark_1_1.mark_pressed.connect(_on_mark_pressed)
 	mark_1_2.mark_pressed.connect(_on_mark_pressed)
-	mark_1_3.mark_pressed.connect(_on_mark_pressed)
+
+	mark_2_0.mark_pressed.connect(_on_mark_pressed)
 	mark_2_1.mark_pressed.connect(_on_mark_pressed)
 	mark_2_2.mark_pressed.connect(_on_mark_pressed)
-	mark_2_3.mark_pressed.connect(_on_mark_pressed)
-	mark_3_1.mark_pressed.connect(_on_mark_pressed)
-	mark_3_2.mark_pressed.connect(_on_mark_pressed)
-	mark_3_3.mark_pressed.connect(_on_mark_pressed)
 	
 func _on_mark_pressed(mark: Mark) -> void:
 	local_player_x.active()
--- Scenes/mark.gd
+++ Scenes/mark.gd
@@ -1,43 +1,24 @@
-extends Button
+extends Node2D
 
 class_name Mark
 
 signal mark_pressed(mark: Mark)
 
-@onready var animation_player = $AnimationPlayer
+@onready var text = $action/text
 
 @export var x: int = 0
 @export var y: int = 0
 
-const _ACTIVE_WHITE_COLOR: Color = Color(255, 255, 255, 1.0)
-const _DEACTIVE_WHITE_COLOR: Color = Color(255, 255, 255, 0)
-
 const _DEFAULT_TEXT: String = "~"
 
-var is_used = false
-
-func _on_pressed() -> void:
-	if !is_used:
-		is_used = true
-		print("Mark:_on_pressed")
+func _on_action_pressed():
+	if !text.visible:
 		mark_pressed.emit(self)
-	
+		
 func active(player_value: String) -> void:
-	print("Mark:active:1")
-	text = player_value
-	print("Mark:active:2")
-	animation_player.play("active")
-	print("Mark:active:3")
-	remove_theme_color_override("font_color")
-	print("Mark:active:4")
-	add_theme_color_override("font_color", _ACTIVE_WHITE_COLOR)
-	print("Mark:active:5")
+	text.text = player_value
+	text.visible = true
 	
 func deactive() -> void:
-	is_used = false
-	text = _DEFAULT_TEXT
-	
-	animation_player.play("deactive")
-	remove_theme_color_override("font_color")
-	add_theme_color_override("font_color", _DEACTIVE_WHITE_COLOR)
-	
+	text.text = _DEFAULT_TEXT
+	text.visible = false
