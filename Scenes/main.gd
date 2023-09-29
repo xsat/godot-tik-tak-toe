103a9b4bd604c29c9a4a88ce32057f35cda496ea
Yurii Antonchyk <xsat.php@gmail.com>
2023-09-30 00:25:20 +0300

    Added change player

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -15,13 +15,12 @@ class_name Main
 @onready var mark_2_1: Mark = $Grid/Mark_2_1 as Mark
 @onready var mark_2_2: Mark = $Grid/Mark_2_2 as Mark
 
-
-var player_value: String
+var player_value: String = "~"
 
 func _ready() -> void:
-	local_player_x.deactive()
-	local_player_o.active()
-	player_value = local_player_o.text
+	print("Grid:_ready")
+	
+	_change_active_player()
 	
 	mark_0_0.mark_pressed.connect(_on_mark_pressed)
 	mark_0_1.mark_pressed.connect(_on_mark_pressed)
@@ -36,12 +35,16 @@ func _ready() -> void:
 	mark_2_2.mark_pressed.connect(_on_mark_pressed)
 	
 func _on_mark_pressed(mark: Mark) -> void:
-	local_player_x.active()
-	local_player_o.deactive()
-	player_value = local_player_x.text
-	
-	print(mark.x)
-	print(mark.y)
-	print("Grid:_on_mark_pressed")
 	mark.active(player_value)
 	
+	_change_active_player()
+	
+func _change_active_player() -> void:
+	if player_value == local_player_x.text:
+		local_player_x.deactive()
+		local_player_o.active()
+		player_value = local_player_o.text
+	else:
+		local_player_x.active()
+		local_player_o.deactive()
+		player_value = local_player_x.text
