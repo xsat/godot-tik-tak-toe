8889e1f3752a4dc1eaddf9a7bb5fcbf019b9d7e5
Yurii Antonchyk <xsat.php@gmail.com>
2023-09-30 14:22:21 +0300

    Added win detection

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -15,6 +15,16 @@ class_name Main
 @onready var mark_2_1: Mark = $Grid/Mark_2_1 as Mark
 @onready var mark_2_2: Mark = $Grid/Mark_2_2 as Mark
 
+const NONE_WIN_POSITION: int = 0
+const FIRST_HORIZONTAL_WIN_POSITION: int = 1
+const SECOND_HORIZONTAL_WIN_POSITION: int = 2
+const THIRD_HORIZONTAL_WIN_POSITION: int = 3
+const FISRT_VERTICAL_WIN_POSITION: int = 4
+const SECOND_VERTICALL_WIN_POSITION: int = 5
+const THIRD_VERTICAL_WIN_POSITION: int = 6
+const FALLING_DIAGONAL_WIN_POSITION: int = 7
+const RISING_DIAGONAL_WIN_POSITION: int = 8
+
 var player_value: String = "~"
 
 func _ready() -> void:
@@ -37,7 +47,11 @@ func _ready() -> void:
 func _on_mark_pressed(mark: Mark) -> void:
 	mark.active(player_value)
 	
-	_change_active_player()
+	var last_win_position: int = _get_player_win_positiion();
+	if last_win_position == NONE_WIN_POSITION:
+		_change_active_player()
+	else:
+		print('win')
 	
 func _change_active_player() -> void:
 	if player_value == local_player_x.text:
@@ -48,3 +62,39 @@ func _change_active_player() -> void:
 		local_player_x.active()
 		local_player_o.deactive()
 		player_value = local_player_x.text
+		
+func _get_player_win_positiion() -> int:
+	if _is_same_value(mark_0_0, mark_0_1, mark_0_2):
+		return FIRST_HORIZONTAL_WIN_POSITION
+		
+	if _is_same_value(mark_1_0, mark_1_1, mark_1_2):
+		return SECOND_HORIZONTAL_WIN_POSITION
+		
+	if _is_same_value(mark_2_0, mark_2_1, mark_2_2):
+		return THIRD_HORIZONTAL_WIN_POSITION
+		
+	if _is_same_value(mark_0_0, mark_1_0, mark_2_0):
+		return FISRT_VERTICAL_WIN_POSITION
+		
+	if _is_same_value(mark_0_1, mark_1_1, mark_2_1):
+		return SECOND_VERTICALL_WIN_POSITION
+		
+	if _is_same_value(mark_0_2, mark_1_2, mark_2_2):
+		return THIRD_VERTICAL_WIN_POSITION
+		
+	if _is_same_value(mark_0_0, mark_1_1, mark_2_2):
+		return FALLING_DIAGONAL_WIN_POSITION
+		
+	if _is_same_value(mark_2_0, mark_1_1, mark_0_2):
+		return RISING_DIAGONAL_WIN_POSITION
+		
+	return NONE_WIN_POSITION
+	
+func _is_same_value(mark_a: Mark, mark_b: Mark, mark_c: Mark) -> bool:
+	if !_is_makrs_used(mark_a, mark_b, mark_c):
+		return false
+	
+	return mark_a.text.text == mark_b.text.text and mark_b.text.text == mark_c.text.text
+
+func _is_makrs_used(mark_a: Mark, mark_b: Mark, mark_c: Mark) -> bool:
+	return mark_a.text.visible && mark_b.text.visible && mark_c.text.visible
